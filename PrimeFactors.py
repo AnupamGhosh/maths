7c626be95aec586ffdadd0801a80077efc5aa5bb
Anupam Ghosh <i_anupam_ghosh@yahoo.co.in>
2015-05-03 11:42:23 +0530

    file created

---
+++ PrimeFactors.py
@@ -0,0 +1,15 @@
+no = input()
+primefactors = []
+def factors(no):
+    factor = 2
+    while factor * factor <= no:
+        if no % factor == 0:
+            e = 0
+            while no % factor == 0:
+                no /= factor
+                e += 1
+            yield factor, e
+        factor += 1
+    if no > 1:
+        yield no, 1
+print [factor for factor in factors(no)]
