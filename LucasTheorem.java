b4a6f3845e950c866a0fb09374db5db3dbcbf291
Anupam Ghosh <i_anupam_ghosh@yahoo.co.in>
2015-08-30 09:51:35 +0530

    removed precomputation of inverse factorial

--- LucasTheorem.java
+++ LucasTheorem.java
@@ -1,16 +1,14 @@
 class LucasTheorem {
     int MOD;
     long[] factorial;
-    long[] invFact;
     
     LucasTheorem(int MOD) {
         this.MOD = MOD;
         factorial = new long[MOD];
         invFact = new long[MOD];
-        factorial[0] = invFact[0] = 1;
+        factorial[0] = 1;
         for (int i = 1; i < MOD; i++) {
             factorial[i] = factorial[i - 1] * i % MOD;
-            invFact[i] = invFact[i - 1] * pow(i, MOD - 2) % MOD;
         }
     }
 
@@ -18,7 +16,7 @@ class LucasTheorem {
         if (n < r) {
             return 0;
         } else if (n < MOD) {
-            return factorial[n] * invFact[r] * invFact[n - r] % MOD;
+            return factorial[n] * pow(factorial[r], MOD - 2) * pow(factorial[n - r], MOD - 2) % MOD;
         }
         return choose(n / MOD, r / MOD) * choose(n % MOD, r % MOD) % MOD; // Lucas Theorem
     }
@@ -42,7 +40,7 @@ public class Main {
         int[] N = {692438152,396973201,371470000,382201820,1079381818,165846332,787591618,403089918,1081222583,370631452};
         int[] R = {346219076,198486600,185735000,191100910,539690909,82923166,393795809,201544959,540611291,185315726};
         for (int i = 0; i < 10; i++) {
-            System.out.println(N + " " + R + " " + combination.choose(N[i], R[i]));
+            System.out.printf("%11d %11d %7d\n", N[i], R[i], combination.choose(N[i], R[i]));
         }
     }   
 }
